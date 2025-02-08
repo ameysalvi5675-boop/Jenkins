1762eeb9b616e170c4a011726305b4ddb976de1c
ameysalvi5675-boop <ameypro157@gmail.com>
2025-02-08 22:36:25 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello from my Python app!"
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)  # Host 0.0.0.0 for Docker
